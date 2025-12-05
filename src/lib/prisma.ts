c05087def2427dfbe07532f3eaff2c5fa0402634
Alvin Sanjaya <alvinsanjaya182@gmail.com>
2025-12-05 21:03:43 +0700

    menambahkan data dan libary unttk membantu jalannya website di belakang layar

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,17 @@
-// src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export default prisma;
+// KITA UBAH BAGIAN INI
+// Alih-alih kosong (), kita masukkan opsi datasources secara manual
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
